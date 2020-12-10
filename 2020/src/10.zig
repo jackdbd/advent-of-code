a92625a174b6dd5572c1bd0ac880125993b7c905
Giacomo Debidda <jackdebidda@gmail.com>
2020-12-10 13:06:21 +0100

    Add part 2 day 10 2020

--- 2020/src/10.zig
+++ 2020/src/10.zig
@@ -1,15 +1,12 @@
 const std = @import("std");
 const utils = @import("utils.zig");
+// const input = @embedFile("inputs/10_minisample.txt");
 // const input = @embedFile("inputs/10_sample.txt");
 const input = @embedFile("inputs/10.txt");
 const fmt = std.fmt;
 const heap = std.heap;
 const log = std.log;
 const mem = std.mem;
-
-fn lessThan(_: void, a: usize, b: usize) bool {
-    return a < b;
-}
 
 fn answer1(allocator: *mem.Allocator) !usize {
     var result: usize = 0;
@@ -26,10 +23,10 @@ fn answer1(allocator: *mem.Allocator) !usize {
 
     // I would have preferred to use only a hash map and sort it, but I still
     // don't know how to do it in zig.
-    std.sort.sort(usize, list.items, {}, lessThan);
+    std.sort.sort(usize, list.items, {}, comptime std.sort.asc(usize));
 
-    var diff_1: usize = 1;
-    var diff_3: usize = 1;
+    var diff_1: usize = 1; // I think this might start from 0 or 1
+    var diff_3: usize = 1; // I think this might start from 1 or 2
     for (list.items) |n, i| {
         if (map.get(n + 1) != null) {
             diff_1 += 1;
@@ -38,12 +35,42 @@ fn answer1(allocator: *mem.Allocator) !usize {
         }
     }
     result = diff_1 * diff_3;
+    // log.debug("diff_1 {}, diff_3 {}", .{diff_1, diff_3});
     return result;
 }
 
+fn maxKey(map: std.AutoHashMap(usize, usize)) usize {
+    var res: usize = 0;
+    var map_it = map.iterator();
+    while (map_it.next()) |v| {
+        if (v.key > res) res = v.key;
+    }
+    return res;
+}
+
 fn answer2(allocator: *mem.Allocator) !usize {
-    var result: usize = 0;
-    return result;
+    var list = std.ArrayList(usize).init(allocator);
+
+    var lines = mem.split(input, "\n");
+    while (lines.next()) |line| {
+        const n = try fmt.parseInt(usize, line, 10);
+        try list.append(n);
+    }
+    std.sort.sort(usize, list.items, {}, comptime std.sort.asc(usize));
+
+    var ways = std.AutoHashMap(usize, usize).init(allocator);
+
+    // v's solutions are ((v-1) + (v-2) + (v-3))'s solutions.\
+    // E.g. when v=7 we take the sum of solutions from v=6, v=5 and v=4
+    try ways.put(0, 1);
+    for (list.items) |v| {
+        const x = ways.get(v - 1) orelse 0;
+        const y = if (v < 2) 0 else ways.get(v - 2) orelse 0;
+        const z = if (v < 3) 0 else ways.get(v - 3) orelse 0;
+        // log.debug("v: {} == x: {}, y: {}, z: {}, x+y+z: {}", .{ v, x, y, z, x + y + z });
+        ways.put(v, (x + y + z)) catch unreachable;
+    }
+    return ways.get(maxKey(ways)).?;
 }
 
 pub fn main() !void {
@@ -73,10 +100,10 @@ test "Day 10, part 1" {
     testing.expectEqual(@intCast(usize, 2760), a);
 }
 
-// test "Day 10, part 2" {
-//     var arena = heap.ArenaAllocator.init(heap.page_allocator);
-//     defer arena.deinit();
-//     const a = try answer2(&arena.allocator);
-//     // testing.expectEqual(@intCast(i32, 8), a);
-//     testing.expectEqual(@intCast(i32, 1235), a);
-// }
+test "Day 10, part 2" {
+    var arena = heap.ArenaAllocator.init(heap.page_allocator);
+    defer arena.deinit();
+    const a = try answer2(&arena.allocator);
+    // testing.expectEqual(@intCast(usize, 19208), a);
+    testing.expectEqual(@intCast(usize, 13816758796288), a);
+}
