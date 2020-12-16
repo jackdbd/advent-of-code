d3f7660fad99ef7b5008b217ad89cfd4e0f95e4b
Giacomo Debidda <jackdebidda@gmail.com>
2020-12-16 17:17:08 +0100

    Add part 2 day 16 2020

--- 2020/src/16.zig
+++ 2020/src/16.zig
@@ -1,11 +1,83 @@
 const std = @import("std");
 const utils = @import("utils.zig");
 // const input = @embedFile("inputs/16_sample.txt");
+// const input = @embedFile("inputs/16_sample2.txt");
 const input = @embedFile("inputs/16.txt");
 const fmt = std.fmt;
 const heap = std.heap;
 const log = std.log;
 const mem = std.mem;
+
+// TODO: refactor part 1 to use the same structs used in part 2. And add docs to functions
+
+const Range = struct {
+    low: usize,
+    high: usize,
+};
+
+const Field = struct {
+    name: []const u8, // e.g. "row", "seat", "departure track"
+    rng0: Range,
+    rng1: Range,
+    const Self = @This();
+
+    fn fromLine(line: []const u8) fmt.ParseIntError!Self {
+        var it = mem.tokenize(line, ":");
+        const i_colon = mem.lastIndexOf(u8, line, ":").?;
+        const name = line[0..i_colon];
+        var it2 = mem.tokenize(line[i_colon + 1 ..], " ");
+
+        var nn = mem.split(it2.next().?, "-");
+        _ = it2.next(); // it's the "or"
+        var mm = mem.split(it2.next().?, "-");
+
+        const n0 = try fmt.parseInt(usize, nn.next().?, 10);
+        const n1 = try fmt.parseInt(usize, nn.next().?, 10);
+        const m0 = try fmt.parseInt(usize, mm.next().?, 10);
+        const m1 = try fmt.parseInt(usize, mm.next().?, 10);
+
+        return Self{
+            .name = name,
+            .rng0 = Range{ .low = n0, .high = n1 },
+            .rng1 = Range{ .low = m0, .high = m1 },
+        };
+    }
+
+    fn isDigitInRange(self: *const Self, d: usize) bool {
+        return ((d >= self.rng0.low and d <= self.rng0.high) or (d >= self.rng1.low and d <= self.rng1.high));
+    }
+};
+
+const Ticket = struct {
+    digits: []usize,
+    string: []const u8,
+    const Self = @This();
+
+    fn fromLine(allocator: *mem.Allocator, line: []const u8) !Self {
+        const n_items = mem.count(u8, line, ",") + 1;
+        const digits = try allocator.alloc(usize, n_items);
+        var it = mem.tokenize(line, ",");
+        var i: usize = 0;
+        while (i < n_items) : (i += 1) {
+            digits[i] = try fmt.parseInt(usize, it.next().?, 10);
+        }
+        return Ticket{ .digits = digits, .string = line };
+    }
+
+    fn isValid(self: *const Self, fields: std.ArrayList(Field)) bool {
+        var invalid_digits: usize = 0;
+        for (self.digits[0..]) |d| {
+            var count: usize = 0;
+            for (fields.items) |field| {
+                if ((d >= field.rng0.low and d <= field.rng0.high) or (d >= field.rng1.low and d <= field.rng1.high)) {
+                    count += 1;
+                }
+            }
+            if (count == 0) invalid_digits += 1;
+        }
+        return if (invalid_digits == 0) true else false;
+    }
+};
 
 const Rule = struct {
     n0: usize,
@@ -14,9 +86,10 @@ const Rule = struct {
     m1: usize,
 };
 
-const TicketMap = std.AutoHashMap([]const u8, Rule);
+const WordRuleMap = std.AutoHashMap([]const u8, Rule);
 
-fn parseRules(map: *TicketMap, line: []const u8) !void {
+// TODO: refactor. This is Field.fromLine()
+fn parseRules(map: *WordRuleMap, line: []const u8) !void {
     var it = mem.tokenize(line, ":");
     const i_colon = mem.lastIndexOf(u8, line, ":").?;
     const word = line[0..i_colon];
@@ -36,7 +109,8 @@ fn parseRules(map: *TicketMap, line: []const u8) !void {
     try map.put(word, Rule{ .n0 = n0, .n1 = n1, .m0 = m0, .m1 = m1 });
 }
 
-fn invalidDigitsInTicket(map: *TicketMap, ticket: []const u8) fmt.ParseIntError!usize {
+// TODO: refactor. This is almost the same as Ticket.isValid()
+fn invalidDigitsInTicket(map: *WordRuleMap, ticket: []const u8) fmt.ParseIntError!usize {
     // log.debug("TICKET {}", .{ticket});
     var invalid_sum: usize = 0;
     var digits = mem.split(ticket, ",");
@@ -61,9 +135,7 @@ fn invalidDigitsInTicket(map: *TicketMap, ticket: []const u8) fmt.ParseIntError!
 
 fn answer1(allocator: *mem.Allocator) !usize {
     var error_rate: usize = 0;
-
-    var map = TicketMap.init(allocator);
-
+    var map = WordRuleMap.init(allocator);
     var parsing_rules = true;
     var parsing_my_ticket = false;
     var parsing_nearby_tickets = false;
@@ -99,16 +171,93 @@ fn answer1(allocator: *mem.Allocator) !usize {
         }
     }
 
-    // var it = map.iterator();
-    // while (it.next()) |e| {
-    //     log.debug("e {}", .{e});
-    // }
-
     return error_rate;
 }
 
+// I don't know how to use a set in zig, so I use a hash map with a value of null
+const CandidatesMap = std.AutoHashMap([]const u8, void);
+
+const PositionCandidatesMap = std.AutoHashMap(usize, CandidatesMap);
+
 fn answer2(allocator: *mem.Allocator) !usize {
-    var result: usize = 0;
+    var result: usize = 1;
+    var fields = std.ArrayList(Field).init(allocator);
+    var my_ticket: Ticket = undefined;
+    var nearby_tickets = std.ArrayList(Ticket).init(allocator);
+
+    var lines = mem.split(input, "\n");
+    while (lines.next()) |line| {
+        if (mem.eql(u8, "", line)) break;
+        try fields.append(try Field.fromLine(line));
+    }
+
+    while (lines.next()) |line| {
+        if (mem.eql(u8, "your ticket:", line)) {
+            continue;
+        } else if (mem.eql(u8, "", line)) {
+            break;
+        }
+        my_ticket = try Ticket.fromLine(allocator, line);
+    }
+
+    while (lines.next()) |line| {
+        if (mem.eql(u8, "nearby tickets:", line)) {
+            continue;
+        }
+        try nearby_tickets.append(try Ticket.fromLine(allocator, line));
+    }
+
+    var map = PositionCandidatesMap.init(allocator);
+    const n_positions = nearby_tickets.items[0].digits.len;
+    const n_tickets = nearby_tickets.items.len;
+
+    var i_pos: usize = 0;
+    var n_invalid_tickets: usize = 0;
+    while (i_pos < n_positions) : (i_pos += 1) {
+        var cmap = CandidatesMap.init(allocator);
+        for (fields.items) |field| {
+            try cmap.put(field.name, .{});
+        }
+        for (nearby_tickets.items) |ticket, i_ticket| {
+            if (ticket.isValid(fields)) {
+                const d = ticket.digits[i_pos];
+                for (fields.items) |field| {
+                    if (!field.isDigitInRange(d)) {
+                        _ = cmap.remove(field.name);
+                    }
+                }
+            } else {
+                if (i_pos == 0) n_invalid_tickets += 1;
+            }
+        }
+        try map.put(i_pos, cmap);
+    }
+    // log.debug("Skipped {} invalid tickets", .{n_invalid_tickets});
+
+    // Keep iterating on `map` (a PositionCandidatesMap) and removing stuff/
+    // Messy, but it works. TODO: think about a recursive approach.
+    var it = map.iterator();
+    while (it.next()) |e| {
+        i_pos = e.key;
+        const cmap = map.get(i_pos).?;
+        var itc = cmap.iterator();
+        if (cmap.count() == 1) {
+            const key = itc.next().?.key; // e.g. "row", "seat"
+            // log.debug("FOUND {} at position {}", .{ key, i_pos });
+            if (mem.startsWith(u8, key, "departure")) {
+                // log.debug("my_ticket[{}]={}", .{ i_pos, my_ticket.digits[i_pos] });
+                result *= my_ticket.digits[i_pos];
+            }
+            // now remove key from all the CandidatesMap
+            var it_cleaner = map.iterator();
+            while (it_cleaner.next()) |ee| {
+                _ = ee.value.remove(key);
+            }
+            _ = map.remove(i_pos);
+            it.index = 0;
+        }
+    }
+
     return result;
 }
 
@@ -135,14 +284,13 @@ test "Day 16, part 1" {
     var arena = heap.ArenaAllocator.init(heap.page_allocator);
     defer arena.deinit();
     const a = try answer1(&arena.allocator);
-    testing.expectEqual(@intCast(usize, 71), a);
+    // testing.expectEqual(@intCast(usize, 71), a);
     testing.expectEqual(@intCast(usize, 18142), a);
 }
 
-// test "Day 16, part 2" {
-//     var arena = heap.ArenaAllocator.init(heap.page_allocator);
-//     defer arena.deinit();
-//     const a = try answer2(&arena.allocator);
-//     // testing.expectEqual(@intCast(i32, 8), a);
-//     testing.expectEqual(@intCast(i32, 1235), a);
-// }
+test "Day 16, part 2" {
+    var arena = heap.ArenaAllocator.init(heap.page_allocator);
+    defer arena.deinit();
+    const a = try answer2(&arena.allocator);
+    testing.expectEqual(@intCast(usize, 1069784384303), a);
+}
