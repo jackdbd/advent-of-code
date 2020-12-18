fdcb13bc0e001697f5258b5bdc805cf78fbd0df5
Giacomo Debidda <jackdebidda@gmail.com>
2020-12-18 17:55:31 +0100

    Add part 2 day 18 2020

--- 2020/src/18.zig
+++ 2020/src/18.zig
@@ -1,16 +1,17 @@
 const std = @import("std");
 const utils = @import("utils.zig");
 // const input = @embedFile("inputs/18_sample.txt");
+// const input = @embedFile("inputs/18_sample2.txt");
 const input = @embedFile("inputs/18.txt");
 const fmt = std.fmt;
 const heap = std.heap;
 const log = std.log;
 const mem = std.mem;
 
-const Op = enum { sum, prod };
+const Op = enum { sum, prod }; // not really necessary but I wanted to try out enums :)
 
-/// Compute a math expression that has no parentheses.
-fn solve(allocator: *mem.Allocator, expr: []const u8) fmt.ParseIntError!usize {
+/// Compute a math expression that has no parentheses and no operator precedence.
+fn solve(expr: []const u8) fmt.ParseIntError!usize {
     std.debug.assert(mem.lastIndexOf(u8, expr, "(") == null);
     std.debug.assert(mem.lastIndexOf(u8, expr, ")") == null);
     // log.debug("solve {}", .{expr});
@@ -33,6 +34,34 @@ fn solve(allocator: *mem.Allocator, expr: []const u8) fmt.ParseIntError!usize {
     return res;
 }
 
+/// Compute a math expression that has no parentheses, but where the + operator
+/// has precedence over the * operator.
+fn solvePart2(allocator: *mem.Allocator, expr: []const u8) MyError!usize {
+    std.debug.assert(mem.lastIndexOf(u8, expr, "(") == null);
+    std.debug.assert(mem.lastIndexOf(u8, expr, ")") == null);
+
+    var i_prod = mem.lastIndexOf(u8, expr, "*");
+    if (i_prod == null) {
+        return solve(expr);
+    }
+
+    var i_sum = mem.lastIndexOf(u8, expr, "+");
+    if (i_sum == null) {
+        return solve(expr);
+    }
+
+    const i_rl = mem.lastIndexOf(u8, expr[0..i_sum.?], "*"); // relative index
+    const i_left = if (i_rl == null) 0 else i_rl.? + 2;
+    const i_rr = mem.indexOf(u8, expr[i_sum.?..], "*"); // relative index
+    const i_right = if (i_rr == null) expr.len else i_sum.? + i_rr.? - 1;
+
+    const left = expr[0..i_left];
+    const n = solve(expr[i_left..i_right]);
+    const right = expr[i_right..];
+    const next_expr = try fmt.allocPrint(allocator, "{}{}{}", .{ left, n, right });
+    return solvePart2(allocator, next_expr);
+}
+
 // TODO: find out how to use ParseIntError, not Overflow and InvalidCharacter
 // ParseIntError = union of Overflow and InvalidCharacter
 const MyError = error{
@@ -45,10 +74,14 @@ const MyError = error{
 };
 
 /// Recursively simplify a math expression, solving operations between parentheses.
-fn simplifyAndSolve(allocator: *mem.Allocator, expr: []const u8) MyError!usize {
+fn simplifyAndSolve(allocator: *mem.Allocator, is_part_1: bool, expr: []const u8) MyError!usize {
     const i = mem.lastIndexOf(u8, expr, "(");
     if (i == null) {
-        return try solve(allocator, expr);
+        if (is_part_1) {
+            return try solve(expr);
+        } else {
+            return try solvePart2(allocator, expr);
+        }
     } else {
         var i_start = i.?;
         var i_stop = expr.len - 1;
@@ -57,12 +90,17 @@ fn simplifyAndSolve(allocator: *mem.Allocator, expr: []const u8) MyError!usize {
             idx = mem.lastIndexOf(u8, expr[i_start..i_stop], ")");
             i_stop = if (idx == null) i_stop else i_start + idx.?;
         }
-        const pre = expr[0..i_start];
-        const n = try solve(allocator, expr[i_start + 1 .. i_stop]);
-        const post = expr[i_stop + 1 ..];
-        const next_expr = try fmt.allocPrint(allocator, "{}{}{}", .{ pre, n, post });
+        const left = expr[0..i_start];
+        var n: usize = undefined;
+        if (is_part_1) {
+            n = try solve(expr[i_start + 1 .. i_stop]);
+        } else {
+            n = try solvePart2(allocator, expr[i_start + 1 .. i_stop]);
+        }
+        const right = expr[i_stop + 1 ..];
+        const next_expr = try fmt.allocPrint(allocator, "{}{}{}", .{ left, n, right });
         // log.debug("next_expr {}", .{next_expr});
-        return try simplifyAndSolve(allocator, next_expr);
+        return try simplifyAndSolve(allocator, is_part_1, next_expr);
     }
 }
 
@@ -70,14 +108,18 @@ fn answer1(allocator: *mem.Allocator) !usize {
     var sum: usize = 0;
     var lines = mem.split(input, "\n");
     while (lines.next()) |line| {
-        sum += try simplifyAndSolve(allocator, line);
+        sum += try simplifyAndSolve(allocator, true, line);
     }
     return sum;
 }
 
 fn answer2(allocator: *mem.Allocator) !usize {
-    var result: usize = 0;
-    return result;
+    var sum: usize = 0;
+    var lines = mem.split(input, "\n");
+    while (lines.next()) |line| {
+        sum += try simplifyAndSolve(allocator, false, line);
+    }
+    return sum;
 }
 
 pub fn main() !void {
@@ -107,10 +149,10 @@ test "Day 18, part 1" {
     testing.expectEqual(@intCast(usize, 12956356593940), a);
 }
 
-// test "Day 18, part 2" {
-//     var arena = heap.ArenaAllocator.init(heap.page_allocator);
-//     defer arena.deinit();
-//     const a = try answer2(&arena.allocator);
-//     testing.expectEqual(@intCast(i32, 8), a);
-//     // testing.expectEqual(@intCast(i32, 1235), a);
-// }
+test "Day 18, part 2" {
+    var arena = heap.ArenaAllocator.init(heap.page_allocator);
+    defer arena.deinit();
+    const a = try answer2(&arena.allocator);
+    // testing.expectEqual(@intCast(usize, 694173), a);
+    testing.expectEqual(@intCast(usize, 94240043727614), a);
+}
